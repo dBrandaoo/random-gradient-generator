daa09ad3b2163383714839aee9c70c41fe586ac9
Diogo Brandão <89142319+dBrandaoo@users.noreply.github.com>
2022-09-03 23:16:58 +0100

    fix: added "0" to the hex chars array

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getGradient() {
-    let hexChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+    let hexChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
     let firstColor = []
     let secondColor = []
 
